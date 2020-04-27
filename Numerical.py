ecd9964dfd11040dfc93f27e523ccfffb60f4cf5
ThejollyWalrus <47336151+ThejollyWalrus@users.noreply.github.com>
2020-04-26 18:59:28 -0700

    Add files via upload

---
+++ Numerical.py
@@ -0,0 +1,28 @@
+#Mason Hamilton & Dylan Nasser
+#CST-305-3:20
+#Professor Ricardo Citro
+#4/26/2020
+#Project 8 Numerical Integration
+#Our grouped worked with Tanner Williams and Jared group
+
+#get the users input for all 3 variables
+part = int(input("Enter the number of partitions for the Riemann sum: "))
+low = float(input("Enter the Lower bound value: "))
+up = float(input("Enter the Upper bound value: "))
+
+#out numerical intergration function
+def Numeric():
+    #creating out first return value
+    def f(x):
+        return 0.1602*x + 1147.1
+
+    value = 0
+    #adjusting our width for the upper and lower bounds
+    width = (up - low) / part
+    #for loop to find the end value of iteration
+    for i in range(1 , part + 1):
+        value += f(low + (i*width)) * width
+    return value
+
+#printing out our finally numeric intergration
+print("Output is: ", Numeric())
